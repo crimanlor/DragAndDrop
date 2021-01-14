a141f965b8c42ce2abae5ad475480a037e3809de
Lorena <criado.manzaneque@gmail.com>
2021-01-14 11:53:20 +0100

    html y css functional

---
+++ script.js
@@ -0,0 +1,34 @@
+const fill = document.querySelector('fill')
+const empties = document.querySelectorAll('empty')
+
+fill.addEventListener('dragstart', )
+fill.addEventListener('dragend', )
+
+for(const empty of empties) {
+
+}
+
+function dragStart() {
+
+    setTimeout()
+}
+
+function dragEnd() {
+    this.
+}
+
+function dragOver(e) {
+    ev.preventDefault();
+}
+
+function dragEnter(e) {
+    ev.preventDefault();
+}
+
+function dragLeave() {
+
+}
+
+function dragDrop() {
+
+}
