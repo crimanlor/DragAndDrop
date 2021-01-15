c07a9ec5f9dc614b2f25ab8319c0e49b22011267
Lorena <criado.manzaneque@gmail.com>
2021-01-15 12:56:06 +0100

    javascript functional and readme

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-Drag and drop in progress 🛠
+# Drag and drop ⬛️🔳⬛️⬛️⬛️
+
+## Prerequisitos y herramientas 🛠
+
+- Visual Studio Code
+- Control de versiones con git
+- Google Chrome
+
+## Tecnologías 💻
+
+- HTML
+- CSS
+- JavaScript
+
+## Demo 📺
+
+![Demo](draganddrop.gif)
+
+## Deploy ejercicio en Github pages 📱
+
+[Drag and Drop deploy](https://github.com/crimanlor/DragAndDrop/deployments/activity_log?environment=github-pages)
--- script.js
+++ script.js
@@ -1,34 +1,39 @@
-const fill = document.querySelector('fill')
-const empties = document.querySelectorAll('empty')
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
 
-fill.addEventListener('dragstart', )
-fill.addEventListener('dragend', )
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
 
-for(const empty of empties) {
-
+for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
 }
 
 function dragStart() {
-
-    setTimeout()
+  this.className += " hold";
+  setTimeout(() => (this.className = " invisible"), 0);
 }
 
 function dragEnd() {
-    this.
+  this.className = "fill";
 }
 
 function dragOver(e) {
-    ev.preventDefault();
+  e.preventDefault();
 }
 
 function dragEnter(e) {
-    ev.preventDefault();
+  e.preventDefault();
+  this.className += " hovered";
 }
 
 function dragLeave() {
-
+  this.className = "empty";
 }
 
 function dragDrop() {
-
+  this.className = "empty";
+  this.append(fill);
 }
